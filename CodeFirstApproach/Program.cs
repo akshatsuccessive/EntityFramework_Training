937f98198eb7e99109f41aeea99d0f92be7eb53e
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-12-08 16:48:24 +0530

    Entity framework, code first approach with Data models Student and Standard

---
+++ CodeFirstApproach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // adding a student
+            using (EFCodeFirstContext context = new EFCodeFirstContext())
+            {
+                var stud = new Student() { FirstName = "Akshat", LastName = "Kumar" };
+                context.Students.Add(stud);
+                context.SaveChanges();
+                Console.WriteLine("Student Added");
+                Console.ReadKey();
+            }
+        }
+    }
+}
