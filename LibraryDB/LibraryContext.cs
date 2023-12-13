9955159cb21b638233bf9c5e0757b2e5e981cea2
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-12-13 17:51:01 +0530

    Create model builder for entities and to establish the relationship between them

--- LibraryDB/LibraryContext.cs
+++ LibraryDB/LibraryContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace EF_Exercise
 {
     internal class LibraryContext : DbContext
     {
-        public LibraryContext() : base("LibraryContext") 
+        public LibraryContext() : base("LibraryContext")
         {
             Database.SetInitializer(new CreateDatabaseIfNotExists<LibraryContext>());
         }
@@ -18,5 +19,22 @@ namespace EF_Exercise
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+            .HasRequired(b => b.Author)
+            .WithMany(a => a.Books)
+            .HasForeignKey(b => b.AuthorId)
+            .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Book>()
+            .HasRequired(b => b.Person)
+            .WithMany(p => p.Books)
+            .HasForeignKey(b => b.PersonId)
+            .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
