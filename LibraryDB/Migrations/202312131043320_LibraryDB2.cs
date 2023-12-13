9f9bb46b30b826125fcb9ca3da0f225d4e7b17cd
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-12-13 17:50:16 +0530

    Adding the migration file

---
+++ LibraryDB/Migrations/202312131043320_LibraryDB2.cs
@@ -0,0 +1,41 @@
+﻿namespace EF_Exercise.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class LibraryDB2 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        AuthorId = c.Int(nullable: false, identity: true),
+                        AuthorName = c.String(),
+                    })
+                .PrimaryKey(t => t.AuthorId);
+            
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        BookId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        AuthorId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.BookId)
+                .ForeignKey("dbo.Authors", t => t.AuthorId)
+                .Index(t => t.AuthorId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Books", "AuthorId", "dbo.Authors");
+            DropIndex("dbo.Books", new[] { "AuthorId" });
+            DropTable("dbo.Books");
+            DropTable("dbo.Authors");
+        }
+    }
+}
---
+++ LibraryDB/Migrations/202312131120266_LibraryDB21.cs
@@ -0,0 +1,32 @@
+﻿namespace EF_Exercise.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class LibraryDB21 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        PersonId = c.Int(nullable: false, identity: true),
+                        PersonName = c.String(),
+                    })
+                .PrimaryKey(t => t.PersonId);
+            
+            AddColumn("dbo.Books", "PersonId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Books", "PersonId");
+            AddForeignKey("dbo.Books", "PersonId", "dbo.People", "PersonId");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Books", "PersonId", "dbo.People");
+            DropIndex("dbo.Books", new[] { "PersonId" });
+            DropColumn("dbo.Books", "PersonId");
+            DropTable("dbo.People");
+        }
+    }
+}
